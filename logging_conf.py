9babb92b5ba1f23f904a2150f41b42f1e69c3053
HongxuChen <leftcopy.chx@gmail.com>
2015-09-26 19:43:02 +0800

    refinement on formatter

--- logging_conf.py
+++ logging_conf.py
@@ -9,6 +9,14 @@ def init_logger():
     logging_yaml = os.path.join(os.path.dirname(__file__), 'logging.yaml')
     with open(logging_yaml) as f:
         data = yaml.load(f)
+    logger_dir = 'log'
+    if not os.path.isdir(logger_dir):
+        os.mkdir(logger_dir)
+    else:
+        for f in os.listdir(logger_dir):
+            fpath = os.path.join(logger_dir, f)
+            if f.endswith('.LOG') and os.path.isfile(f):
+                os.remove(fpath)
     logging.config.dictConfig(data)
 
 
